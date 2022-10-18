ddb3ebdf66efeb302e1e3715bd3782e0172bf23a
Dawid Kosiński <tofik1432@gmail.com>
2022-10-18 20:35:28 +0200

    BST basics and delete method

--- datastructures/tests/test_trees.py
+++ datastructures/tests/test_trees.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from ..trees import BinaryTreeNode
+from ..trees import BinarySearchTreeNode, BinaryTreeNode
 
 
 class BinaryTreeNodeTestCase(TestCase):
@@ -27,7 +27,6 @@ class BinaryTreeNodeTestCase(TestCase):
         return root
 
     def test_inorder(self):
-        # expected_value = [33, 10, 18, 1, 3, 21, 128]
         expected_value = [25, 33, 10, 18, 300, 1, 3, 21, 5, 128, 82]
 
         result = self.root.get_inorder()
@@ -35,7 +34,6 @@ class BinaryTreeNodeTestCase(TestCase):
         self.assertEqual(expected_value, result)
 
     def test_preorder(self):
-        # expected_value = [1, 10, 33, 18, 21, 3, 128]
         expected_value = [1, 10, 33, 25, 18, 300, 21, 3, 128, 5, 82]
 
         result = self.root.get_preorder()
@@ -43,7 +41,6 @@ class BinaryTreeNodeTestCase(TestCase):
         self.assertEqual(expected_value, result)
 
     def test_postorder(self):
-        # expected_value = [33, 18, 10, 3, 128, 21, 1]
         expected_value = [25, 33, 300, 18, 10, 3, 5, 82, 128, 21, 1]
 
         result = self.root.get_postorder()
@@ -81,3 +78,113 @@ class BinaryTreeNodeTestCase(TestCase):
 
         self.assertFalse(found)
         self.assertEqual(expected_path, path)
+
+
+class BinarySearchTreeNodeTestCase(TestCase):
+    """TestCase for BinarySearchTreeNode."""
+
+    def setUp(self) -> None:
+        values = [18, 12, 30, 10, 15, 28, 45, 1, 11, 14, 16, 20, 29, 40, 60]
+        self.root = self.create_tree(values)
+
+    @staticmethod
+    def create_tree(values: list) -> BinarySearchTreeNode:
+        root = BinarySearchTreeNode(values[0])
+        for value in values[1:]:
+            root.insert(value)
+        return root
+
+    def test_inorder(self):
+        expected_order = [1, 10, 11, 12, 14, 15, 16, 18, 20, 28, 29, 30, 40, 45, 60]
+
+        result = self.root.get_inorder()
+
+        self.assertEqual(expected_order, result)
+
+    def test_preorder(self):
+        expected_order = [18, 12, 10, 1, 11, 15, 14, 16, 30, 28, 20, 29, 45, 40, 60]
+
+        result = self.root.get_preorder()
+
+        self.assertEqual(expected_order, result)
+
+    def test_postorder(self):
+        expected_order = [1, 11, 10, 14, 16, 15, 12, 20, 29, 28, 40, 60, 45, 30, 18]
+
+        result = self.root.get_postorder()
+
+        self.assertEqual(expected_order, result)
+
+    def test_breadth_first_search_finds_value(self):
+        expected_path = [18, 12, 30, 10, 15]
+
+        found, path = self.root.breadth_first_search(15)
+
+        self.assertTrue(found)
+        self.assertEqual(expected_path, path)
+
+    def test_breadth_first_search_doesnt_find_value(self):
+        expected_path = [18, 12, 30, 10, 15, 28, 45, 1, 11, 14, 16, 20, 29, 40, 60]
+
+        found, path = self.root.breadth_first_search(180)
+
+        self.assertFalse(found)
+        self.assertEqual(expected_path, path)
+
+    def test_depth_first_search_finds_value(self):
+        expected_path = [18, 12, 15]
+
+        found, path = self.root.depth_first_search(15)
+
+        self.assertTrue(found)
+        self.assertEqual(expected_path, path)
+
+    def test_depth_first_search_doesnt_find_value(self):
+        expected_path = [18, 12, 15, 16]
+
+        found, path = self.root.depth_first_search(17)
+
+        self.assertFalse(found)
+        self.assertEqual(expected_path, path)
+
+    def test_depth_first_search_iterative_finds_value(self):
+        expected_path = [18, 12, 15]
+
+        found, path = self.root.depth_first_search_iterative(15)
+
+        self.assertTrue(found)
+        self.assertEqual(expected_path, [n.value for n in path])
+
+    def test_depth_first_search_iterative_doesnt_find_value(self):
+        expected_path = [18, 12, 15, 16]
+
+        found, path = self.root.depth_first_search_iterative(17)
+
+        self.assertFalse(found)
+        self.assertEqual(expected_path, [n.value for n in path])
+
+    def test_delete_leaf_node(self):
+        expected_order = [1, 10, 12, 14, 15, 16, 18, 20, 28, 29, 30, 40, 45, 60]
+
+        self.root.delete(11)
+
+        result = self.root.get_inorder()
+        self.assertEqual(expected_order, result)
+
+    def test_delete_node_with_one_child(self):
+        values = [18, 12, 30, 10, 15, 28, 45, 1, 11, 14, 16, 20, 29, 40]
+        root = self.create_tree(values)
+        expected_order = [1, 10, 11, 12, 14, 15, 16, 18, 20, 28, 29, 30, 40]
+
+        root.delete(45)
+
+        result = root.get_inorder()
+        self.assertEqual(expected_order, result)
+
+    def test_delete_node_with_two_children(self):
+        expected_order = [1, 10, 11, 12, 14, 16, 18, 20, 28, 29, 30, 40, 45, 60]
+
+        self.root.delete(15)
+
+        result = self.root.get_inorder()
+        self.assertEqual(expected_order, result)
--- datastructures/trees.py
+++ datastructures/trees.py
@@ -77,3 +77,100 @@ class BinaryTreeNode:
             if found:
                 return found, path
         return False, path
+
+
+class BinarySearchTreeNode(BinaryTreeNode):
+    def insert_left(self, value: int):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not allow manual insertion!"
+            f" Use .insert() method."
+        )
+
+    def insert_right(self, value: int):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not allow manual insertion!"
+            f" Use .insert() method."
+        )
+
+    def insert(self, value: int):
+        if value <= self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTreeNode(value)
+
+        if value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTreeNode(value)
+
+    def depth_first_search(self, value: int) -> tuple[bool, list[int]]:
+        """Depth-first search using recursive binary search algorithm."""
+        found, path = False, [self.value]
+        if self.value == value:
+            return True, path
+        if value < self.value:
+            if self.left:
+                found, traversed_path = self.left.depth_first_search(value)
+                path.extend(traversed_path)
+            else:
+                return False, path
+        if value > self.value:
+            if self.right:
+                found, traversed_path = self.right.depth_first_search(value)
+                path.extend(traversed_path)
+            else:
+                return False, path
+        return found, path
+
+    def depth_first_search_iterative(self, value: int) -> tuple[bool, list]:
+        """Depth-first search using iterative binary search algorithm."""
+        node = self
+        path = []
+        while node is not None:
+            path.append(node)
+            if node.value == value:
+                break
+            if value < node.value:
+                node = node.left
+            elif value > node.value:
+                node = node.right
+        if node:
+            return True, path
+        return False, path
+
+    def delete(self, value: int, parent=None):
+        """Delete a node from the tree."""
+        found, path = self.depth_first_search_iterative(value)
+        if not found:
+            raise ValueError(f"{value} not in Tree")
+        if len(path) == 1:
+            parent, node = parent, path[0]
+        else:
+            parent, node = path[-2:]
+
+        # leaf node
+        if not any([node.left, node.right]):
+            if parent.left == node:
+                parent.left = None
+            else:
+                parent.right = None
+            del node
+        # single child
+        elif (node.left and not node.right) or (not node.left and node.right):
+            if parent.left == node:
+                parent.left = node.left if node.left else node.right
+            elif parent.right == node:
+                parent.right = node.right if node.right else node.left
+            del node
+        # two children
+        else:
+            smallest_node = node.right.find_minimum()
+            node.value = smallest_node.value
+            node.right.delete(smallest_node.value, node)
+
+    def find_minimum(self):
+        if self.left:
+            return self.left.find_minimum()
+        return self
